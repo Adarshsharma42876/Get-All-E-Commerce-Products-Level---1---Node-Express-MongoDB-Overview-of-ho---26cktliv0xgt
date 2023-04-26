44b8516f679cc9dad00f2c9d88f2a76060f842a9
ADARSH SHARMA <120009672+Adarshsharma42876@users.noreply.github.com>
2023-04-26 10:06:57 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,23 @@ const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 // Middlewares
 app.use(express.json());
 
+
+
 // Write GET endpoint for sending all the products to client here
 // Endpoint - /api/v1/products
+app.get("/api/v1/products", (req, res) => {
+    if (!products) {
+        return res.status(404).json({
+            "message": "Product not found"
+        });
+    }
+    return res.status(200).json({
+        "status": "success",
+        "message": "Product fetched successfully",
+        data:{
+            product: products
+        }
+    })
+})
 
 module.exports = app;
